eefb94fb80390d3449e45348c19e51135331b603
陈士力 <chenshili@z-trip.cn>
2023-03-24 16:44:43 +0800

    update: fix

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')()
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use(
+express.use(
   '*',
   createProxyMiddleware({
     target: 'https://api.openai.com',
     changeOrigin: true,
   })
 );
-
-app.listen(3000);
 
 express.listen(3000, () => {
   console.log('agent start')
