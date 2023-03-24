dab90b34414bbdd53f3950559d936329e5540a1f
陈士力 <chenshili@z-trip.cn>
2023-03-24 17:09:40 +0800

    update: fix

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express')()
-
+const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-express.use(createProxyMiddleware({
-  target: 'https://api.openai.com',
-  changeOrigin: true,
-}));
+const app = express();
+const port = 3000;
 
-express.listen(3000, () => {
-  console.log('agent start')
-})
+// ChatGPT API endpoint
+const chatgptApiUrl = 'https://api.openai.com';
+
+// Proxy middleware for /chat
+const chatProxy = createProxyMiddleware('/v1', {
+  target: chatgptApiUrl,
+  changeOrigin: true,
+  pathRewrite: {
+    '/v1': '/v1'
+  },
+});
+
+// Use proxy middleware
+app.use(chatProxy);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
